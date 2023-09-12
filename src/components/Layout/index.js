c12f3b3dd1750ece3e1e5bddf2c599c1d5de1498
gurusamyp <gurumprp@gmail.com>
2023-09-12 13:12:50 +0530

    Home page addedd

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+import  Sidebar from '../Sidebar';
+const Layout = () => {
+    return (
+        <><Sidebar /></>
+    );
+}
+
+export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import './index.scss';
+import {Link, NavLink} from 'react-router-dom'
+import logoS from '../../assets/images/logo-s.png'
+import logoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+const Sidebar = () => (
+    
+        <div className="nav-bar">
+          <Link className='logo' to='\' >
+            <img src={logoS} alt="logo" />
+            <img className='sub-logo' src={logoSubtitle} alt="Slobodan" />
+          </Link>
+          <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+              <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className="about-link" to='/about'>
+              <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' to='/contact' className='contact-link'>
+              <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+          </nav>
+
+     
+        </div>
+    
+);
+
+export default Sidebar;
